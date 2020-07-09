6798af653ddab57dfbf2e57fae3cc104a3050384
Ebo Lee <yiboli.coding@gmail.com>
2020-07-09 17:56:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -19,10 +19,10 @@ int = 20
 
 def countdown_with_sleep(int)
   while int > 0
-      puts "#{int} SECOND(S)!"
-      int -= 1
-      sleep(1)
-    end 
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+  end 
       return "HAPPY NEW YEAR!"
   end
 end 
