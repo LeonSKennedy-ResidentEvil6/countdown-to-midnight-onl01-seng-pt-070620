031c35bf9e576bd3ac82cb87728be7a2348d10ae
Ebo Lee <yiboli.coding@gmail.com>
2020-07-09 17:42:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,9 @@
 def countdown(number)
     number = 10
     while number < 10
+        puts "#{number} SECOND(S)!"
     number -= 1
-    puts "#{number} SECOND(S)!"
+
     puts HAPPY NEW YEAR!""
   end 
 end
